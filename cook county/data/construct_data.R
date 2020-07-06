a2743268222d0a6ea813f8b4a15268cafed6b32a
Eric Langowski <langowski@uchicago.edu>
2020-07-06 10:42:31 -0500

    changing cook county data structure

--- cook county/data/construct_data.R
+++ cook county/data/construct_data.R
@@ -1,9 +1,11 @@
 library(tidyverse)
 library(data.table)
 
+file_loc <- "cook county/data/"
+
 #characteristics
-characteristics <- fread("characteristics.csv", colClasses = "character")
-addresses <- fread("addresses.csv", colClasses = "character")
+characteristics <- fread(paste0(file_loc, "characteristics.csv" ), colClasses = "character")
+addresses <- fread(paste0(file_loc, "addresses.csv"), colClasses = "character")
 
 characteristics <- characteristics %>% 
   filter(`Tax Year` == 2019) %>% 
@@ -25,7 +27,7 @@ addresses <- addresses %>%
 joined <- characteristics %>% left_join(addresses, by=c("Property Address" = "CMPADDABRV", "PIN10"))
 
 #add most recent assessment/sale
-assessments <- fread("assessments.csv", colClasses = "character") %>% 
+assessments <- fread(paste0(file_loc, "assessments.csv"), colClasses = "character") %>% 
   filter(YEAR == 2019)
 
 assessments <- 
@@ -33,7 +35,7 @@ assessments <-
                          WON_CCAO = ifelse(CCAO_APPEAL == "1", ifelse(CHANGED == "YES", 1, 0), NA)) %>% 
   select(PIN, CLASS, NBHD, TOWN, CERTIFIED, `TOWN NAME`, CCAO_APPEAL, WON_CCAO)
 
-sales <- fread("sales.csv", colClasses = "character") %>% 
+sales <- fread(paste0(file_loc, "sales.csv"), colClasses = "character") %>% 
   select(PIN, `Sale Price`, `Sale Year`) %>% 
   filter(`Sale Price` > 1000) %>%
   arrange(-as.numeric(`Sale Year`)) %>%
@@ -44,15 +46,15 @@ joined2 <- assessments %>% left_join(sales)
 full <- joined %>% left_join(joined2)
 
 
-good_classes <- c(202, 203, 204, 205, 206, 207, 208, 209, 210, 234, 278, 295)
-bad_classes <- c(211, 212, 299)
+res_classes <- c(202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 234, 278, 295)
+condos <- c(299)
 
 full <- full %>% mutate(stories_recode = case_when(`Type of Residence` %in% c("1", "4", "5", "6", "7", "8", "9") ~ "1",
                                                    TRUE ~ `Type of Residence`),
                         basement_recode = case_when(Basement %in% c("2", "3", "4") ~ "0",
                                                     Basement == "1" ~ "1")
-)
+) %>% select(-CLASS, -NBHD, -TOWN)
 
-fwrite(full %>% filter(CLASS %in% good_classes), "combined.csv")
-fwrite(full %>% filter(CLASS %in% bad_classes), "otherclasses.csv")
+fwrite(full %>% filter(`Property Class` %in% res_classes), paste0(file_loc, "cooksf.csv"))
+fwrite(full %>% filter(`Property Class` %in% condos), paste0(file_loc, "cookcondos.csv"))
 
