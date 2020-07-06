23d187e6e90050a75d1f58bf01d71946444bb5df
Eric Langowski <langowski@uchicago.edu>
2020-07-06 10:42:46 -0500

    adding code to create comparables for cook county

---
+++ ptap_site/api/computils.py
@@ -0,0 +1,37 @@
+from math import radians, degrees, sin, cos, asin, acos, sqrt
+from numpy import searchsorted, sort
+
+#utility functions
+def ecdf(x):
+    x = sort(x)
+    n = len(x)
+    def _ecdf(v):
+        # side='right' because we want Pr(x <= v)
+        return (searchsorted(x, v, side='right') + 1) / n
+    return _ecdf
+
+def filter_on(df, col, val, range_val, debug=False):
+    print(df.shape)
+    
+    if (col in ['Wall Material', 'stories_recode', 'basement_recode', 'Garage indicator']) & (range_val == 'Match'):
+        if debug:
+            print('filtering ' + col + ' ' + str(val) + ' on ' + str(range_val))
+        return df[df[col] == val]
+    elif col == 'Distance':
+        if debug:
+            print('filtering on distance less than', str(range_val), 'miles away')
+        df['Distance'] = df.apply(lambda x: great_circle(val[0], val[1], x.Longitude, x.Latitude), axis=1) 
+        return df[df[col] < range_val]
+    else:
+        if debug:
+            print('filtering ' + col + ' ' + str(val) + ' +/- ' + str(range_val))
+        return df[df[col].between(val - range_val, val + range_val)]
+    
+def great_circle(lon1, lat1, lon2, lat2):
+    '''
+    https://medium.com/@petehouston/calculate-distance-of-two-locations-on-earth-using-python-1501b1944d97
+    '''
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+    return 3958.756 * (
+        acos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(lon1 - lon2))
+    )
---
+++ ptap_site/api/cook_sf.py
@@ -0,0 +1,66 @@
+from computils import filter_on, ecdf #module
+
+def comps_cook_sf(targ, cook_sf, multiplier):
+    ###
+    #constants
+    age_dif = 10 * multiplier
+    build_dif = 0.05 * targ['Building Square Feet'].values[0] * multiplier
+    land_dif = 0.25 * targ['Land Square Feet'].values[0] * multiplier
+    rooms_dif = 1 * multiplier
+    bedroom_dif = 1 * multiplier
+    
+    wall_material = "Match" #wall material 1=wood, 2=masonry, 3=wood&masonry, 4=stucco
+    stories = "Match" #1, 2, or 3 stories
+    basement = "Match" #1 full, 0 partial
+    garage_ind = "Match" #1 any garage 0 no garage
+    
+    distance_filter = 1 * multiplier #miles
+    
+    cook_sf_cols = ['PIN', 'Property Class', 'Age', 'Building Square Feet', 'Land Square Feet', 
+    'Rooms', 'Bedrooms', 'Wall Material', 'stories_recode', 'basement_recode', 'Garage indicator',
+    'Distance', 'CERTIFIED']
+    ###
+       
+    new = cook_sf[cook_sf['PIN'] != targ['PIN'].values[0]]
+    new = filter_on(new, 'Age', targ['Age'].values[0], age_dif)
+    new = filter_on(new, 'Building Square Feet', targ['Building Square Feet'].values[0], build_dif)
+    new = filter_on(new, 'Land Square Feet', targ['Land Square Feet'].values[0], land_dif)
+    new = filter_on(new, 'Rooms', targ['Rooms'].values[0], rooms_dif)
+    new = filter_on(new, 'Bedrooms', targ['Bedrooms'].values[0], bedroom_dif)
+    
+    new = filter_on(new, 'Wall Material', targ['Wall Material'].values[0], wall_material)
+    new = filter_on(new, 'stories_recode', targ['stories_recode'].values[0], stories)
+    new = filter_on(new, 'basement_recode', targ['basement_recode'].values[0], basement)
+    new = filter_on(new, 'Garage indicator', targ['Garage indicator'].values[0], garage_ind)
+
+    new = filter_on(new, 'Distance', (targ['Longitude'].values[0], targ['Latitude'].values[0]), distance_filter)
+    
+    print(new.shape)
+        
+    return(new[cook_sf_cols])
+
+def process_one_pin(input_data, cook_sf, multiplier=1):
+    # for now, input only as pin 
+    # for now, let's do cook sf only
+    # eventually we will need to determine if a pin/address is cook sf, cook condos, detroit
+    target_pin = '16052120090000' #replace with actual input
+    targ = cook_sf[cook_sf['PIN'] == target_pin]
+
+    cur_comps = comps_cook_sf(targ, cook_sf, multiplier)
+    
+    if(multiplier > 3): #no comps found with parameters stop search
+        return ''
+    elif(cur_comps.shape[0] < 10): # find more comps
+        return process_one_pin(input_data, multiplier*1.25)
+    else: # return best comps
+        dist_weight = 1
+        valuation_weight = 3
+        
+        cur_comps['dist_dist'] = ecdf(cur_comps.Distance)(cur_comps.Distance)
+        cur_comps['val_dist'] = ecdf(cur_comps.CERTIFIED)(cur_comps.CERTIFIED)
+        cur_comps['score'] = dist_weight * cur_comps['dist_dist'] + valuation_weight * cur_comps['val_dist']
+        cur_comps = cur_comps.sort_values(by=['score'])
+        output = {}
+        output['target_pin'] = targ.to_json(orient='records')
+        output['comparables'] = cur_comps.to_json(orient='records') 
+        return output
