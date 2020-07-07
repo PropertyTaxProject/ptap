aad367b863dedb0791221e30b02bf04bc9b1cd49
Eric Langowski <langowski@uchicago.edu>
2020-07-06 19:01:17 -0500

    mvp gen attachments

--- ptap_site/api/api.py
+++ ptap_site/api/api.py
@@ -2,7 +2,7 @@ import time
 from flask import Flask, request
 import pandas as pd
 
-from cook_sf import process_one_pin #module
+from cook_sf import process_one_pin, submit_cook_sf #module
 
 #load data
 cook_sf = pd.read_csv('../../cook county/data/cooksf.csv',
@@ -33,9 +33,25 @@ def get_comps(form_data):
         comparables : [{char1:val1,...},{char1:val1,...}] #sorted by best to worst
     }
     """
-    data_json = process_one_pin(form_data, cook_sf)
-    #print(data_json)
+    data_json = process_one_pin(form_data, cook_sf, 25)
+    finalize_appeal('') ##TMP
 
     return(data_json)
 
 
+def finalize_appeal(form_data):
+    # convert form input to filed appeal
+    # for now, let's do cook sf only
+    # eventually we will need to deal with cook sf, cook condos, detroit
+    '''
+    Input
+    {
+        target_pin : [{char1:val1,...}],
+        comparables : [{char1:val1,...},{char1:val1,...}] #selected comparables only
+    }
+    '''
+
+    response_dict = submit_cook_sf(process_one_pin(form_data, cook_sf, 10))
+
+    return {'request_status': time.time(),
+    'response': response_dict}
