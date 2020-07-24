3bcf9d1a3dd307704cfa006b23c4d9a063fce35a
Austin Zhang <austinzhang1018@gmail.com>
2020-07-24 13:25:36 -0400

    added error handling

--- ptap_site/api/api.py
+++ ptap_site/api/api.py
@@ -23,11 +23,17 @@ def handle_form():
     page1_data = request.json
     print('PAGE DATA', request.json)
     print('REQUEST OBJECT', request)
-    response_dict = get_comps(page1_data)
-    resp = jsonify({'request_status': time.time(),
-    'response': response_dict})
+    try:
+        response_dict = get_comps(page1_data)
+        resp = jsonify({'request_status': time.time(),
+        'response': response_dict})
+    except Exception as e:
+        resp = jsonify({'error': str(e)})
+        print(e)
+
     resp.headers.add('Access-Control-Allow-Origin', '*')
     return resp
+
 
 
 @app.route('/api_v1/submit2', methods=['POST'])
