d836ccc00e20db8ee7fdff43d0e5379beab8fd4c
Austin Zhang <austinzhang1018@gmail.com>
2020-08-12 03:45:56 -0400

    formatting

--- ptap_site/frontend/src/components/landing-page.jsx
+++ ptap_site/frontend/src/components/landing-page.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button, Space } from 'antd';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 const PTAPHeader = () => (
   <>
