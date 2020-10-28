17bcc0d5483a4a94dca54d08a8007d2e6844c151
Eric Langowski <erhlango@gmail.com>
2020-10-28 18:04:34 -0500

    adding google sheet logging

--- api/mainfct.py
+++ api/mainfct.py
@@ -6,7 +6,21 @@ from datetime import datetime
 from fuzzywuzzy import process
 import io
 import os
+import json
+import gspread
+from google.oauth2 import service_account
 from .computils import comps_cook_sf, comps_detroit_sf, ecdf, prettify_detroit, prettify_cook
+
+#open connection with google sheets
+credentials = service_account.Credentials.from_service_account_file(
+    './.env/ptap-904555bfffb0.json',
+    scopes = ["https://spreadsheets.google.com/feeds",
+          "https://www.googleapis.com/auth/spreadsheets",
+          "https://www.googleapis.com/auth/drive.file",
+          "https://www.googleapis.com/auth/drive"])
+
+client = gspread.authorize(credentials)
+gsheet = client.open("ptap-log").sheet1
 
 def address_candidates(input_data, data_dict, cutoff_info):
     output = {}
@@ -296,15 +310,30 @@ def generate_detroit_sf(comp_submit):
 def record_log(uuid_val, process_step_id, exception, form_data):
     new = {}
     new['uuid'] = uuid_val
-    new['process_step_id'] = process_step_id,
-    new['exception'] = exception
+    new['process_step_id'] = process_step_id
+    new['exception'] = repr(exception)
     new['time'] = time.time()
 
-    tmp = pd.DataFrame.from_dict(new)
+    tmp = pd.DataFrame(new, index=[0])
     tmp = pd.concat([tmp, pd.json_normalize(form_data).drop('uuid', axis=1, errors='ignore')], axis=1)
     p = 'tmp_log.csv'
 
-    tmp.to_csv(p, index=False, mode='a')#, header=not os.path.exists(p))
+    tmp.to_csv(p, index=False, mode='a')
+
+    #google sheets log
+    for i, j in form_data.items():
+        if i == 'target_pin':
+            new[i] = j['PIN']
+        elif i == 'comparables':
+            cnt = 1
+            for comp in j:
+                new['comp_' + str(cnt)] = comp['PIN']
+                cnt += 1
+        else:
+            new[i] = j
+
+    test = [list(new.keys())] + [list(new.values())]
+    gsheet.append_rows(test)
 
     if exception:
         print(tmp.T)
