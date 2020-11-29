d15d2ba33888c6d05acd75427a75e6f59d5c3cc4
Eric Langowski <erhlango@gmail.com>
2020-11-29 11:32:00 -0600

    restructure

--- api/database/make_db.py
+++ api/database/make_db.py
@@ -2,9 +2,9 @@ import sqlite3
 import pandas as pd
 
 data_dict = {}
-data_dict['cook_sf'] =  pd.concat([pd.read_csv('./../cooksf1.csv', dtype={'PIN':str, 'st_num':str}), 
-                                   pd.read_csv('./../cooksf2.csv', dtype={'PIN':str, 'st_num':str})])
-data_dict['detroit_sf'] = pd.read_csv('./../detroit_sf.csv', dtype={'st_num':str})
+data_dict['cook_sf'] =  pd.concat([pd.read_csv('api/database/cooksf1.csv', dtype={'PIN':str, 'st_num':str}), 
+                                   pd.read_csv('api/database/cooksf2.csv', dtype={'PIN':str, 'st_num':str})])
+data_dict['detroit_sf'] = pd.read_csv('api/database/detroit_sf.csv', dtype={'st_num':str})
 
 con = sqlite3.connect('data.sqlite')
 
