46e59a60d130a65578dba69827bd3f38380b3d71
Eric Langowski <erhlango@gmail.com>
2020-12-05 14:49:19 -0600

    update first two pages per comments
    see google doc

--- frontend/src/landing/get-started.jsx
+++ frontend/src/landing/get-started.jsx
@@ -4,9 +4,18 @@ import { Link } from 'react-router-dom';
 
 const PTAPHeader2 = () => (
     <>
-      <h2>Who we Are</h2>
-      <p>Who are we? What does this app do?</p>
-      <p>Click the “Agree” button below if you want to appeal your property tax assessment with support from a UMLS homeowner advocate.</p>
+      <h2>The Process</h2>
+      <ul>
+        <li>Step 1: Complete this online application by February 10, 2020. If you have any problems with the application, call our hotline or email us (
+          <a href='mailto:law-propertytax@umich.edu?subject=Request for Assistance'>law-propertytax@umich.edu</a>) and our staff can help you.</li>
+        <li>Step 2: Once you complete the application, our team will receive a draft appeal letter.</li>
+        <li>Step 3: Our team will call you to review the appeal letter.</li>
+        <li>Step 4: Our team will send you a “Letter of Authorization,” which you must sign in order for us to represent you and send the appeal in on your behalf.</li>
+        <li>Step 5: On February 15, 2021, our team will submit the necessary documents at the Assessor’s Review (the first stage of the appeal process).</li>
+        <li>Step 6: On March 8, 2021, our team will file the appeal documents at the March Board of Review (the second stage of the appeal process).</li>
+        <li>Step 7: Sometime in March, the City will send you its decision.</li>
+        <li>Step 8: Our team will follow up with you to discuss other housing-related resources.</li>
+      </ul>
       <Space>
         <Button><Link to="/selectregion">Agree</Link></Button>
       </Space>
--- frontend/src/landing/landing-page.jsx
+++ frontend/src/landing/landing-page.jsx
@@ -4,14 +4,42 @@ import { Link } from 'react-router-dom';
 
 const PTAPHeader = () => (
   <>
-    <h2>Welcome to the Property Tax Appeal Project&apos;s automated appeal system.</h2>
-
+    <h1>Welcome to the Property Tax Appeal Project&apos;s automated appeal system.</h1>
     <h2>The Tax Foreclosure Crisis</h2>
-    <p>Information on the crisis.</p>
-    <h2>Why should you appeal?</h2>
-    <p>Answer the question.</p>
+
+    <ul>
+      <li>According to the Michigan Constitution, cities cannot assess a property at more than 50% of its market value.</li>
+      <li>So, if your home is worth $50,000, the “Assessed Value” on your property tax bill cannot be more than $25,000.</li>
+      <li>But, between 2009-2015, the City of Detroit illegally inflated the “Assessed Value” for&nbsp;
+        <a href='https://southerncalifornialawreview.com/2018/01/02/stategraft-article-by-bernadette-atuahene-timothy-r-hodge/'
+           target='_blank' rel="noopener noreferrer">55% to 85%</a> of its properties.
+      </li>
+      <li>In 2019, the problem continued. The City illegally inflated the “Assessed Value” for 84% of the lowest valued homes.</li>
+      <li>As a result, the City has overtaxed Detroit homeowners by at least&nbsp;
+        <a href='https://www.detroitnews.com/story/news/local/detroit-city/housing/2020/01/09/detroit-homeowners-overtaxed-600-million/2698518001/'
+           target='_blank' rel="noopener noreferrer">$600 million</a> and one out of every 3 homes went through property tax foreclosure.
+      </li>
+      <li><a href='https://papers.ssrn.com/sol3/papers.cfm?abstract_id=3202860' target='_blank' rel="noopener noreferrer">25% of the property tax foreclosures</a> of homes 
+      lower in value would not have happened without these illegally inflated property tax assessments</li>
+    </ul>
+    <p>Check out a short video below and visit the <a href='https://illegalforeclosures.org/' target='_blank' rel="noopener noreferrer">
+      Coalition for Property Tax Justice website</a> for more information!</p>
+    <iframe title="Illegal Foreclosures" width="560" height="315" src="https://www.youtube.com/embed/J1wlRYB3p7E" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <h2>Why Should You Appeal Your Property Tax Assessment?</h2>
+    <ul>
+      <li>You can make sure that your property is correctly valued, which may lower your property taxes.</li>
+      <li>Appealing is FREE -- our team does not charge any fee for our services!</li>
+      <li>Appealing takes very little time  – Our team does a lot of the leg work for you.</li>
+      <li>It is easy – you just need to complete our short online application OR call our hotline and our team will do it for you.</li>
+    </ul>
+    <h2>What is the Property Tax Appeal Project?</h2>
+    <ul>
+      <li>It is a free legal service provided by the Coalition for Property Tax Justice and the University of Michigan Law School.</li>
+      <li>Our team is here both to help you file the appeal and to understand the appeal process.</li>
+      <li>During the 2020 appeals process, 100% of our client’s appeals were successful!</li>
+    </ul>
     <Space>
-      <Button><Link to="/getstarted">Get Started with Your Appeal!</Link></Button>
+      <Button type="primary"><Link to="/getstarted">Click here to get started</Link></Button>
     </Space>
   </>
 );
