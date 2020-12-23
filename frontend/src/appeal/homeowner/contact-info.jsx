5b2ce91368540007a7f08c67a80287f53366dc95
Eric Langowski <erhlango@gmail.com>
2020-12-22 22:07:27 -0600

    adding homeowner info to form

--- frontend/src/appeal/homeowner/contact-info.jsx
+++ frontend/src/appeal/homeowner/contact-info.jsx
@@ -41,7 +41,9 @@ const tailFormItemLayout = {
   },
 };
 
-const HomeownerInfo = (props) => {
+const HomeownerInfo = () => {
+  const [form] = Form.useForm();
+
   const [showMailingAddr, updateMailingAddr] = useState(false);
   const [showAltContact, updateAltContact] = useState(false);
   return (
@@ -52,208 +54,212 @@ const HomeownerInfo = (props) => {
           <p>How should we contact you?</p>
         </Col>
       </Row>
-      <Form.Item
-        name="name"
-        label="Full Name"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your full name!',
-          },
-        ]}
+      <Form
+      form={form}
+      {...formItemLayout}
       >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        name="email"
-        label="Email"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your Email!',
-            type: 'email',
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-    
-      <Form.Item
-        name="phone"
-        label="Phone Number"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your phone number!',
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-
-      <Form.Item
-        name="address"
-        label="Street Address"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your street address!',
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-
-      <Form.Item
-        name="city"
-        label="City"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your city!',
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-
-      <Form.Item
-        name="state"
-        label="State"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your State!',
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-
-      <Form.Item
-        name="zip"
-        label="Zip Code"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your zip code!',
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
+        <Form.Item
+          name="name"
+          label="Full Name"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your full name!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="email"
+          label="Email"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your Email!',
+              type: 'email',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
       
-      <Form.Item
-        name="mailingsame"
-        label="My mailing address is the same as my street address"
-        rules={[
-          {
-            required: true,
-            message: 'Please respond!',
-          },
-        ]}
-      >
-        <Radio.Group onChange={e => updateMailingAddr(e.target.value)}>
-          <Radio value={'Yes'}>Yes</Radio>
-          <Radio value={'No'}>No</Radio>
-        </Radio.Group>
-      </Form.Item>
+        <Form.Item
+          name="phone"
+          label="Phone Number"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your phone number!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item
-        name="mailingaddress"
-        label="Enter your Mailing Address"
-        style={showMailingAddr === 'No' ? { display: ''} : {display: 'none'}}
-      >
-        {showMailingAddr === 'No' && <Input placeholder="Please enter your mailing address" />}
-      </Form.Item>
+        <Form.Item
+          name="address"
+          label="Street Address"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your street address!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item
-        name="altcontact"
-        label="Did someone help you fill out this form?"
-        rules={[
-          {
-            required: true,
-            message: 'Please respond!',
-          },
-        ]}
-      >
-        <Radio.Group onChange={e => updateAltContact(e.target.value)}>
-          <Radio value={'Yes'}>Yes</Radio>
-          <Radio value={'No'}>No</Radio>
-        </Radio.Group>
-      </Form.Item>
+        <Form.Item
+          name="city"
+          label="City"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your city!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item
-        name="altcontactname"
-        label={<p>Enter <strong><i>their</i></strong> name</p>}
-        style={showAltContact === 'Yes' ? { display: ''} : {display: 'none'}}
-      >
-        {showAltContact === 'Yes' && <Input placeholder="Please enter their name" />}
-      </Form.Item>
+        <Form.Item
+          name="state"
+          label="State"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your State!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item
-        name="altcontactrelationship"
-        label={<p>What is your relationship with <strong><i>them</i></strong>?</p>}
-        style={showAltContact === 'Yes' ? { display: ''} : {display: 'none'}}
-      >
-        {showAltContact === 'Yes' && <Input placeholder="Please enter your relationship" />}
-      </Form.Item>
+        <Form.Item
+          name="zip"
+          label="Zip Code"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your zip code!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        
+        <Form.Item
+          name="mailingsame"
+          label="My mailing address is the same as my street address"
+          rules={[
+            {
+              required: true,
+              message: 'Please respond!',
+            },
+          ]}
+        >
+          <Radio.Group onChange={e => updateMailingAddr(e.target.value)}>
+            <Radio value={'Yes'}>Yes</Radio>
+            <Radio value={'No'}>No</Radio>
+          </Radio.Group>
+        </Form.Item>
 
-      <Form.Item
-        name="altcontactemail"
-        label={<p>What is <strong><i>their</i></strong> email address?</p>}
-        style={showAltContact === 'Yes' ? { display: ''} : {display: 'none'}}
-      >
-        {showAltContact === 'Yes' && <Input placeholder="Please enter their email" />}
-      </Form.Item>
+        <Form.Item
+          name="mailingaddress"
+          label="Enter your Mailing Address"
+          style={showMailingAddr === 'No' ? { display: ''} : {display: 'none'}}
+        >
+          {showMailingAddr === 'No' && <Input placeholder="Please enter your mailing address" />}
+        </Form.Item>
 
-      <Form.Item
-        name="altcontactphone"
-        label={<p>What is <strong><i>their</i></strong> phone number?</p>}
-        style={showAltContact === 'Yes' ? { display: ''} : {display: 'none'}}
-      >
-        {showAltContact === 'Yes' && <Input placeholder="Please enter their phone number" />}
-      </Form.Item>
+        <Form.Item
+          name="altcontact"
+          label="Did someone help you fill out this form?"
+          rules={[
+            {
+              required: true,
+              message: 'Please respond!',
+            },
+          ]}
+        >
+          <Radio.Group onChange={e => updateAltContact(e.target.value)}>
+            <Radio value={'Yes'}>Yes</Radio>
+            <Radio value={'No'}>No</Radio>
+          </Radio.Group>
+        </Form.Item>
 
-      <Form.Item
-        name="altcontactpreferred"
-        label={<p>What is <strong><i>their</i></strong> preferred contact method?</p>}
-        style={showAltContact === 'Yes' ? { display: ''} : {display: 'none'}}
-      >
-        {showAltContact === 'Yes' && 
+        <Form.Item
+          name="altcontactname"
+          label={<p>Enter <strong><i>their</i></strong> name</p>}
+          style={showAltContact === 'Yes' ? { display: ''} : {display: 'none'}}
+        >
+          {showAltContact === 'Yes' && <Input placeholder="Please enter their name" />}
+        </Form.Item>
+
+        <Form.Item
+          name="altcontactrelationship"
+          label={<p>What is your relationship with <strong><i>them</i></strong>?</p>}
+          style={showAltContact === 'Yes' ? { display: ''} : {display: 'none'}}
+        >
+          {showAltContact === 'Yes' && <Input placeholder="Please enter your relationship" />}
+        </Form.Item>
+
+        <Form.Item
+          name="altcontactemail"
+          label={<p>What is <strong><i>their</i></strong> email address?</p>}
+          style={showAltContact === 'Yes' ? { display: ''} : {display: 'none'}}
+        >
+          {showAltContact === 'Yes' && <Input placeholder="Please enter their email" />}
+        </Form.Item>
+
+        <Form.Item
+          name="altcontactphone"
+          label={<p>What is <strong><i>their</i></strong> phone number?</p>}
+          style={showAltContact === 'Yes' ? { display: ''} : {display: 'none'}}
+        >
+          {showAltContact === 'Yes' && <Input placeholder="Please enter their phone number" />}
+        </Form.Item>
+
+        <Form.Item
+          name="altcontactpreferred"
+          label={<p>What is <strong><i>their</i></strong> preferred contact method?</p>}
+          style={showAltContact === 'Yes' ? { display: ''} : {display: 'none'}}
+        >
+          {showAltContact === 'Yes' && 
+            <Radio.Group>
+              <Radio value='Phone'>Phone</Radio>
+              <Radio value='Email'>Email</Radio>
+              <Radio value='Both'>Both/No Preference</Radio>
+            </Radio.Group>
+          }
+        </Form.Item>
+
+        <Form.Item
+          name="preferred"
+          label="What is your preferred contact method?"
+          rules={[
+            {
+              required: true,
+              message: 'Please mark your preferred method!',
+              whitespace: true,
+            },
+          ]}
+        >
           <Radio.Group>
             <Radio value='Phone'>Phone</Radio>
             <Radio value='Email'>Email</Radio>
             <Radio value='Both'>Both/No Preference</Radio>
           </Radio.Group>
-        }
-      </Form.Item>
+        </Form.Item>
 
-      <Form.Item
-        name="preferred"
-        label="What is your preferred contact method?"
-        rules={[
-          {
-            required: true,
-            message: 'Please mark your preferred method!',
-            whitespace: true,
-          },
-        ]}
-      >
-        <Radio.Group>
-          <Radio value='Phone'>Phone</Radio>
-          <Radio value='Email'>Email</Radio>
-          <Radio value='Both'>Both/No Preference</Radio>
-        </Radio.Group>
-      </Form.Item>
-
-      <Form.Item
-        name="heardabout"
-        label="How did you hear about us?"
-      >
-        <Input placeholder='Enter how you heard about us.'></Input>
-      </Form.Item>
-
+        <Form.Item
+          name="heardabout"
+          label="How did you hear about us?"
+        >
+          <Input placeholder='Enter how you heard about us.'></Input>
+        </Form.Item>
+      </Form>
     </>
   )
 };
