ebad6ba6b992c77a68608baf1050bc1e8ba9ed8e
Eric Langowski <erhlango@gmail.com>
2020-12-24 17:10:19 -0600

    Update email.py

--- api/email.py
+++ api/email.py
@@ -2,11 +2,12 @@ import os
 from flask_mail import Message
 
 def detroit_submission_email(mail, data):
+    #email to ptap account with info
     name = data['name']
     addr = data['target_pin']['Address']
     recip_ls = [os.environ.get('MAIL_DEFAULT_RECEIVER')]
 
-    subj = 'Submission: ' + name + ' (' + addr + ')'
+    subj = 'Property Tax Appeal Project Submission: ' + name + ' (' + addr + ')'
     body = "<b>A submission has been received. </b><br>" + \
         "Name: " +  name + "<br>" + \
         "Address: " + addr + "<br>" + \
@@ -21,3 +22,16 @@ def detroit_submission_email(mail, data):
         f.read())
 
     mail.send(msg)
+
+    #receipt to user
+    recip_ls.append(data['email'])
+
+    body = "Dear " + name + ',<br><br>' + \
+        "The Property Tax Appeal Project has received a submission for " + addr + "." + \
+        "An advocate will reach out to you to finalize your application. If you have any questions until then, " + \
+        "please contact us @. <br><br> Thank you, <br><br>The Property Tax Appeal Project Automated System"
+    
+    msg2 = Message(subj, recipients=recip_ls)
+    msg2.html = body
+
+    mail.send(msg2)
