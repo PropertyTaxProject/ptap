549d324c1109511c5db135cb44a146b02b02203a
Eric Langowski <erhlango@gmail.com>
2020-12-24 13:20:32 -0600

    refactor

--- frontend/src/appeal/pages/review-property.jsx
+++ frontend/src/appeal/pages/review-property.jsx
@@ -5,15 +5,13 @@ import {
   Row,
   Col,
   Space,
-  Card,
   Divider,
   Input,
   Radio
 } from 'antd';
+import PropertyInfo from '../shared/property-info';
 
 const { TextArea } = Input;
-const re = /(\b[a-z](?!\s))/g;
-const createTitle = (title) => title.replace('_', ' ').replace(re, (x) => x.toUpperCase());
 
 const formItemLayout = {
   labelCol: {
@@ -35,45 +33,6 @@ const formItemLayout = {
   },
 };
 
-// displays the target property information
-const PropertyInfo = (props) => {
-  const { targetProperty } = props;
-  const { propInfo } = props;
-  const { cols } = props;
-  const gridStyle = {
-    width: `${Math.round(100 / cols)}%`,
-    textAlign: 'center',
-  };
-  const characteristics = Object.entries(targetProperty).filter(([title, description]) => (
-    title !== '' && description !== '' && title !== 'PIN' && title !== 'Distance'
-  ));
-  characteristics.sort(([title1], [title2]) => {
-    const t1 = title1.toLowerCase();
-    const t2 = title2.toLowerCase();
-    if (t1 > t2) { return 1; }
-    if (t1 < t2) { return -1; }
-    return 0;
-  });
-  return (
-    <>
-      <Row>
-        <Col xs={{ span: 24, offset: 0 }} sm={{ span: 24, offset: 0 }}>
-          <h1>Your Property</h1>
-          <p>Below is the data that we have on file for your property.</p>
-          <p>{propInfo}</p>
-          <br />
-        </Col>
-      </Row>
-      {characteristics.map(([title, description]) => (
-        <Card.Grid hoverable={false} style={gridStyle}>
-          <Row><b>{createTitle(title)}</b></Row>
-          <Row><p>{description}</p></Row>
-        </Card.Grid>
-      ))}
-    </>
-  );
-};
-
 const Characteristics = (props) => {
   const {
     targetProperty,
@@ -84,7 +43,6 @@ const Characteristics = (props) => {
 
   const [form] = Form.useForm();
   const [showCharInput, updateCharInput] = useState(false);
-
 
   const onFinish = (info) => {
     console.log('Received values of form: ', info);
