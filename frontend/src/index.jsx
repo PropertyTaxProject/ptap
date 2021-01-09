ad7e64acd3bc5db93a2621655b2b6f8dc62b6562
Eric Langowski <erhlango@gmail.com>
2021-01-09 11:19:44 -0600

    working in theory

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,6 +1,6 @@
 import 'antd/dist/antd.css';
 import './index.css';
-import React from 'react';
+import React, {useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import { Layout } from 'antd';
 import {
@@ -27,45 +27,50 @@ history.listen(location => {
     ReactGA.pageview(location.pathname);
 });
 
-const Page = () => (
+const Page = () => {
+  useEffect( () => {
+    ReactGA.pageview(window.location.pathname);
+  }, []);
+  return(
   <Router history={history}>
-    <Layout className="layout">
-      <Header />
-      <Content style={{ padding: '0 3vw' }}>
-        <div className="site-layout-content">
-          <Switch>
-            <Route
-              path="/detroit"
-              render={() => <DetroitLandingPage />}
-            />
-            <Route
-              path="/detroitappeal"
-              render={() => <Appeal city="detroit" />}
-            />
-            <Route
-              path="/cook"
-              render={() => <Appeal city="chicago" />}
-            />
-            <Route 
-              path='/completedappeal' 
-              render={() => <FinalPage />}
-            />
-            <Route 
-              path='/illegalforeclosures' 
-              component={() => { 
-                window.location.href = 'https://illegalforeclosures.org/'; return null;}}
-            />
-            <Route
-              path="/"
-              render={() => <SelectRegion />}
-            />
-          </Switch>
-        </div>
-      </Content>
-      <Footer style={{ textAlign: 'center' }}>Call 313-438-8698 with any questions. Please leave a message if we don't pick up and we will return your call within 48 hours.</Footer>
-    </Layout>
+  <Layout className="layout">
+    <Header />
+    <Content style={{ padding: '0 3vw' }}>
+      <div className="site-layout-content">
+        <Switch>
+          <Route
+            path="/detroit"
+            render={() => <DetroitLandingPage />}
+          />
+          <Route
+            path="/detroitappeal"
+            render={() => <Appeal city="detroit" />}
+          />
+          <Route
+            path="/cook"
+            render={() => <Appeal city="chicago" />}
+          />
+          <Route 
+            path='/completedappeal' 
+            render={() => <FinalPage />}
+          />
+          <Route 
+            path='/illegalforeclosures' 
+            component={() => { 
+              window.location.href = 'https://illegalforeclosures.org/'; return null;}}
+          />
+          <Route
+            path="/"
+            render={() => <SelectRegion />}
+          />
+        </Switch>
+      </div>
+    </Content>
+    <Footer style={{ textAlign: 'center' }}>Call 313-438-8698 with any questions. Please leave a message if we don't pick up and we will return your call within 48 hours.</Footer>
+  </Layout>
   </Router>
-);
+  )
+};
 
 ReactDOM.render(
   <React.StrictMode>
