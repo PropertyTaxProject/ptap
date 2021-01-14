6bb15463c386c8510d815e1f24feb73833d0b2f0
Eric Langowski <erhlango@gmail.com>
2021-01-13 20:17:08 -0600

    new template!!

--- api/submitappeal.py
+++ api/submitappeal.py
@@ -67,43 +67,53 @@ def submit_detroit_sf(comp_submit, mail):
         'assessed_value' : 'Assessed Value',
         'total_acre' : 'Acres',
         'total_floorarea' : 'Floor Area',
-        'total_sqft' : 'SqFt'
+        'total_sqft' : 'Square Footage (Abv. Ground)',
+        'Age' : 'Year Built',
+        'Exterior' : 'Exterior Material',
+        'Distance' : 'Dist.',
+        'Stories (not including basement)' : 'Number of Stories',
     }
     t_df = pd.DataFrame([comp_submit['target_pin']])
     comps_df = pd.DataFrame(comp_submit['comparables'])
     pin_av = t_df.assessed_value[0]
     pin = t_df.PIN[0]
-    comps_avg = comps_df.assessed_value.mean()
+    comps_avg = comps_df['Sale Price'].map(lambda x: float(x[1:].replace(',', ''))).mean()
 
     #rename cols
     t_df = t_df.rename(columns=rename_dict)
-    comps_df = comps_df.rename(columns=rename_dict).drop(['score'], axis=1)
+    comps_df = comps_df.rename(columns=rename_dict)
+
+    #tbl cols
+    target_cols = ['Baths', 'Square Footage (Abv. Ground)', 'Year Built', \
+        'Exterior Material', 'Number of Stories']
+    
+    comp_cols = ['Address', 'Dist.', 'Sale Price', 'Sale Date'] + target_cols
 
     #generate docx
     output_name = 'api/tmp_data/' + pin + \
         ' Protest Letter Updated ' +  datetime.today().strftime('%m_%d_%y') + '.docx'
     comp_submit['output_name'] = output_name
-    doc = DocxTemplate("api/template_files/detroit_template.docx")
+    doc = DocxTemplate("api/template_files/detroit_template_2021.docx")
 
-    allinfo = ''
+    allinfo = []
     skip_ls = ['target_pin', 'comparables', 'output_name']
     for key, val in comp_submit.items():
         if key not in skip_ls:
-            allinfo += str(key) + ': ' + str(val) + "<w:br/>"
+            allinfo.append([key, val])
 
     context = {
         'pin' : pin,
         'owner' : comp_submit['name'],
         'address' : comp_submit['address'],
         'formal_owner' : comp_submit['name'],
-        'current_sev' : '${:,.2f}'.format(pin_av),
-        'current_faircash' : '${:,.2f}'.format(pin_av * 2),
-        'contention_sev' : '${:,.2f}'.format(comps_avg),
-        'contention_faircash' : '${:,.2f}'.format(comps_avg * 2),
-        'target_labels' : list(t_df.columns),
-        'target_contents' : t_df.to_numpy().tolist(),
-        'comp_labels' : list(comps_df.columns),
-        'comp_contents' : comps_df.to_numpy().tolist(),
+        'current_sev' : '{:,.0f}'.format(pin_av),
+        'current_faircash' : '${:,.0f}'.format(pin_av * 2),
+        'contention_sev' : '{:,.0f}'.format(comps_avg / 2),
+        'contention_faircash' : '${:,.0f}'.format(comps_avg),
+        'target_labels' : target_cols,
+        'target_contents' : t_df[target_cols].to_numpy().tolist(),
+        'comp_labels' : comp_cols,
+        'comp_contents' : comps_df[comp_cols].to_numpy().tolist(),
         'allinfo' : allinfo
             }
         
