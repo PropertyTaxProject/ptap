acdd5d54a0bd4d0db133efd12ca89aa7ddeb4afa
Eric Langowski <erhlango@gmail.com>
2021-01-16 22:05:46 -0600

    steps to email

--- api/email.py
+++ api/email.py
@@ -26,9 +26,18 @@ def detroit_submission_email(mail, data):
 
     #receipt to user
     body = "Dear " + name + ',<br><br>' + \
-        "The Property Tax Appeal Project has received a submission for " + addr + "." + \
-        " An advocate will reach out to you to finalize your application. If you have any questions until then, " + \
-        "please contact us via phone at 313-438-8698 or via email at law-propertytax@umich.edu. <br><br> Thank you, <br><br>The Property Tax Appeal Project Automated System"
+        "The Property Tax Appeal Project has received a submission for " + addr + ". " + \
+        "An advocate will reach out to you to finalize your application. If you have any questions until then, " + \
+        "please contact us via phone at 313-438-8698 or via email at law-propertytax@umich.edu. <br><br> " + \
+        """Steps in Appeals Process:<ul>
+        <li><b>Step 1</b>: Complete this online application by <b>February 15, 2020</b>.</li>
+        <li><b>Step 2</b>: An advocate will call you to review your Application and will let you know whether we can help you.</li>
+        <li><b>Step 3</b>: If your Application is accepted, an advocate will work with you to complete your appeal documents.</li>
+        <li><b>Step 4</b>: Your advocate will submit your documents to the Assessor’s Review by <b>February 22, 2021</b></li>
+        <li><b>Step 5</b>: Your advocate will submit your documents to the March Board of Review by <b>March 8, 2021</b></li>
+        <li><b>Step 6</b>: Before <b>June of 2021</b>, the Board of Review will send you a letter notifying you whether your home's assessed value is reduced because of your appeal.</li>
+        </ul>""" + \
+        "<br><br> Thank you, <br><br>The Property Tax Appeal Project Automated System"
     msg2 = Message(subj, recipients=submit_email, reply_to=uofm[0], cc=uofm, bcc=ptap)
     msg2.html = body
     mail.send(msg2)
