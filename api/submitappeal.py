fcc15fa94d26377b6118262856f95a47de3ea1dd
Eric Langowski <33432469+erhla@users.noreply.github.com>
2021-08-10 10:18:53 -0500

    add cook submit

--- api/submitappeal.py
+++ api/submitappeal.py
@@ -2,56 +2,123 @@ from datetime import datetime
 import io
 from docxtpl import DocxTemplate
 import pandas as pd
-from .email import detroit_submission_email
+from .email import detroit_submission_email, cook_submission_email
 from .logging import record_final_submission
 from .dataqueries import get_pin
 
 def submit_cook_sf(comp_submit, mail):
+    '''
+    Output:
+    Word Document
+    '''
+    download = False #boolean to turn download on/off
+
     rename_dict = {
-        'PIN' : 'Pin',
         'assessed_value' : 'Assessed Value',
-        'building_sqft' : 'Building',
-        'land_sqft' : 'Land',
+        'building_sqft' : 'Square Footage',
+        'Exterior' : 'Exterior Material',
+        'Distance' : 'Dist.',
     }
 
     t_df = pd.DataFrame([comp_submit['target_pin']])
     comps_df = pd.DataFrame(comp_submit['comparables'])
+
     pin_av = t_df.assessed_value[0]
-    comps_avg = comps_df.assessed_value.mean()
     pin = t_df.PIN[0]
+    comps_avg = comps_df['assessed_value'].mean()
 
     #rename cols
     t_df = t_df.rename(columns=rename_dict)
-    comps_df = comps_df.rename(columns=rename_dict).drop(['score'], axis=1)
+    comps_df = comps_df.rename(columns=rename_dict)
 
-    #generate appeal narrative
-    output_name = 'api/tmp_data/' + pin + ' Appeal Narrative.docx'
+    #tbl cols
+    target_cols = ['Beds', 'Square Footage', 'Age', \
+        'Exterior Material', 'Stories']
+    
+    comp_cols = ['Address', 'Dist.'] + target_cols #+ ['Sale Price', 'Sale Year']
 
-    doc = DocxTemplate("api/template_files/cook_template.docx")
+    #check for char input
+    if 'characteristicsinput' not in comp_submit:
+        comp_submit['characteristicsinput'] = 'NO STRUCTURAL DAMAGE REPORTED'
+
+    #generate docx
+    output_name = 'api/tmp_data/' + pin + \
+        ' Protest Letter Updated ' +  datetime.today().strftime('%m_%d_%y') + '.docx'
+    comp_submit['output_name'] = output_name
+    doc = DocxTemplate("api/template_files/dentons_cook_template.docx")
+
+    allinfo = []
+    propinfo = []
+    #skip_ls = ['target_pin', 'comparables', 'output_name']
+    #for key, val in comp_submit.items():
+    #    if key not in skip_ls:
+    #        allinfo.append([key, val])
+
+    #for i, j in get_pin('detroit', pin).to_dict(orient='records')[0].items():
+    #    propinfo.append([i, j])
+
     context = {
         'pin' : pin,
-        'target_av' : '${:,.2f}'.format(pin_av),
-        'comp_av' : '${:,.2f}'.format(comps_avg),
-        'target_labels' : list(t_df.columns),
-        'target_contents' : t_df.to_numpy().tolist(),
-        'comp_labels' : list(comps_df.columns),
-        'comp_contents' : comps_df.to_numpy().tolist()
+        'address' : comp_submit['address'],
+        'homeowner_name' : comp_submit['name'],
+        'assessor_av' : '{:,.0f}'.format(pin_av),
+        'assessor_mv' : '${:,.0f}'.format(pin_av * 10),
+        'contention_av' : '{:,.0f}'.format(comps_avg / 10),
+        'contention_mv' : '${:,.0f}'.format(comps_avg),
+        'target_labels' : target_cols,
+        'target_contents' : t_df[target_cols].to_numpy().tolist(),
+        'comp_labels' : comp_cols,
+        'comp_contents' : comps_df[comp_cols].to_numpy().tolist(),
+        'damage_descript' : comp_submit['characteristicsinput'],
+        'allinfo' : allinfo,
+        'propinfo' : propinfo
             }
+
+    print(context)
+        
     doc.render(context)
     doc.save(output_name)
 
     output = {}
+    if download:
+        # also save a byte object to return
+        file_stream = io.BytesIO()
+        doc.save(file_stream) # save to stream
+        file_stream.seek(0) # reset pointer to head
+        output['file_stream'] = file_stream
+    '''
+    # update submission log
+    targ = get_pin('detroit', pin)
+    
+    if comp_submit['validcharacteristics'] == 'No':
+        c_flag = 'Yes. Homeowner Input: ' + comp_submit['characteristicsinput']
+    else:
+        c_flag = 'No'
+    
+    sub_dict = {
+        'Client Name' : comp_submit['name'],
+        'Address' : comp_submit['address'],
+        'Taxpayer of Record' : targ['taxpayer_1'].to_string(index=False),
+        'PIN' : pin,
+        'Phone Number' : comp_submit['phone'],
+        'Email Address' : comp_submit['email'],
+        'Preferred Contact Method' : comp_submit['preferred'],
+        'PRE' : targ['homestead_'].to_string(index=False),
+        'Eligibility Flag' : comp_submit['eligibility'],
+        'Characteristics Flag': c_flag,
+        'SEV' : str(pin_av),
+        'TV' : targ['taxable_va'].to_string(index=False),
+        'CV' : str(comps_avg)
+    }
 
-    #appeal narrative to file stream
-    file_stream = io.BytesIO()
-    doc.save(file_stream) # save to stream
-    file_stream.seek(0) # reset pointer to head
-    output['file_stream'] = file_stream
+    log_url = record_final_submission(sub_dict)
+    comp_submit['log_url'] = log_url
+    '''
+    comp_submit['log_url'] = 'tmp'
 
-    #generate comps csv
-    #hypen_pin = pin[0:2] + "-" + pin[2:4] + "-" + pin[4:7] + "-" + pin[7:10] + "-" + pin[10:]
-    #comp_csv_name = "api/tmp_data/Comparable PINs for " + hypen_pin + ".csv"
-    #comps_df.to_csv(comp_csv_name, index=False)
+
+    # send email
+    cook_submission_email(mail, comp_submit)
 
     return output
 
