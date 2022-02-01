efc6a7f0779ee8885c2a5b38ac26c396f22b598d
Eric Langowski <33432469+erhla@users.noreply.github.com>
2022-02-01 17:50:57 -0600

    update steps

--- frontend/src/landing/final-page.jsx
+++ frontend/src/landing/final-page.jsx
@@ -13,10 +13,11 @@ const PTAPHeader4 = () => (
     <ul>
       <li> <b>&#10003; You are Here!</b> <b>Step 1</b>: Complete this online application by <b>February 15, 2022</b>.</li>
       <li><b>Step 2</b>: An advocate will call you to review your Application and will let you know whether we can help you.</li>
-      <li><b>Step 3</b>: If your Application is accepted, an advocate will work with you to complete your appeal documents.</li>
-      <li><b>Step 4</b>: Your advocate will submit your documents to the Assessor’s Review by <b>February 22, 2022</b></li>
-      <li><b>Step 5</b>: Your advocate will submit your documents to the March Board of Review by <b>March 8, 2022</b></li>
-      <li><b>Step 6</b>: Before <b>June of 2022</b>, the Board of Review will send you a letter notifying you whether your home's assessed value is reduced because of your appeal.</li>
+      <li><b>Step 3</b>: If your Application is accepted, you will need to sign a Letter of Authorization form. The Project will provide you with this form.</li>
+      <li><b>Step 4</b>: If your Application is accepted, an advocate will work with you to complete your appeal documents.</li>
+      <li><b>Step 5</b>: Your advocate will submit your documents to the Assessor’s Review by <b>February 22, 2022.</b></li>
+      <li><b>Step 6</b>: Your advocate will submit your documents to the March Board of Review by <b>March 8, 2022.</b></li>
+      <li><b>Step 7</b>: Before <b>June of 2022</b>, the Board of Review will send you a letter notifying you whether your home's assessed value is reduced because of your appeal.</li>
     </ul>
     <Space>
       <Button type="primary"><Link to="/illegalforeclosures">See more information on our website</Link></Button>
