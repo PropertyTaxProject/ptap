532c268e4e9e517949166ce30bdeb1615069edaf
Eric Langowski <33432469+erhla@users.noreply.github.com>
2022-02-01 17:51:18 -0600

    update steps and change text

--- frontend/src/appeal/pages/eligibility-requirements.jsx
+++ frontend/src/appeal/pages/eligibility-requirements.jsx
@@ -5,7 +5,8 @@ import {
   Input,
   Button,
   Table,
-  Radio
+  Radio,
+  Badge
 } from 'antd';
 
 var submitted = false;
@@ -16,10 +17,11 @@ const theProcess = (
     <li><b>Step 1</b>: Complete this online application by <b>February 15, 2022</b>. If you are unable to complete the Application or have questions, call or text our hotline (313-438-8698) or email us &nbsp; 
       <a href='mailto:law-propertytax@umich.edu?subject=Request for Assistance'>(law-propertytax@umich.edu)</a>.</li>
     <li><b>Step 2</b>: An advocate will call you to review your Application and will let you know whether we can help you.</li>
-    <li><b>Step 3</b>: If your Application is accepted, an advocate will work with you to complete your appeal documents.</li>
-    <li><b>Step 4</b>: Your advocate will submit your documents to the Assessor’s Review by <b>February 22, 2022</b></li>
-    <li><b>Step 5</b>: Your advocate will submit your documents to the March Board of Review by <b>March 8, 2022</b></li>
-    <li><b>Step 6</b>: Before <b>June of 2022</b>, the Board of Review will send you a letter notifying you whether your home's assessed value is reduced because of your appeal.</li>
+    <li><b>Step 3</b>: If your Application is accepted, you will need to sign a Letter of Authorization form. The Project will provide you with this form.</li>
+    <li><b>Step 4</b>: If your Application is accepted, an advocate will work with you to complete your appeal documents.</li>
+    <li><b>Step 5</b>: Your advocate will submit your documents to the Assessor’s Review by <b>February 22, 2022.</b></li>
+    <li><b>Step 6</b>: Your advocate will submit your documents to the March Board of Review by <b>March 8, 2022.</b></li>
+    <li><b>Step 7</b>: Before <b>June of 2022</b>, the Board of Review will send you a letter notifying you whether your home's assessed value is reduced because of your appeal.</li>
   </ul>
 );
 
@@ -106,7 +108,7 @@ const PinLookup = (props) => {
         scrollToFirstError
         autoComplete="off"
       >
-        <p style={{ width: '350px' }}>Enter your street number and street name and select your property from the table.</p>
+        <p style={{ width: '400px' }}>Enter your street number and street name below and then hit search. After searching from your home, hit the <b>Select button</b> next to your address</p>
 
         <Input.Group compact>
           <Form.Item style={{ width: '100px' }} name="st_num" rules={[{ required: true, message: 'Street name is required.' }]}>
@@ -154,24 +156,26 @@ const Lookup = (props) => {
 
   return (
     <>
+      <p>
+      <b>Disclaimer:</b> Completing this Application does not guarantee that the Project will be able to represent you. 
+      The information you provide will help the Project determine if we can assist you. After completing this Application, 
+      the Project will try to contact you three times. If we don’t hear from you after three attempts, we will remove you from our list.
+      </p>
       <h2>The Appeals Process</h2>  
       <p>
-      The below outlines a  general overview of the appeals process. 
-      Completing this Application does not guarantee that our Project will be able to help you. 
-      The information you provide as part of the Application process will help us determine whether we can assist you. 
+      Here is a general overview of the appeals process:
       </p>
       {theProcess}
       <h2>Who is Eligible for the Project's Services?</h2> 
       <p>
-        We only serve owner-occupied, single-family homes. 
-        We also prioritize helping homeowners of low-value homes, i.e. homes worth $50,000 and less. 
+      You are eligible for services if you meet all of these criteria:
       </p>
-      <PinLookup
-        city={city}
-        logPin={logPin}
-        logUuid={logUuid}
-        setRecord={setRecord}
-      />
+      <ul>
+        <li>You own a home in the City of Detroit.</li>
+        <li>You occupy that home as your permanent residence.</li>
+        <li>Your home is worth $100,000 or less.</li>
+      </ul>
+
       <Form
         form={form}
         name="Eligibility"
@@ -202,6 +206,13 @@ const Lookup = (props) => {
             <Radio value='No'>No</Radio>
           </Radio.Group>
         </Form.Item>
+        <h2>Find Your Home</h2>
+        <PinLookup
+        city={city}
+        logPin={logPin}
+        logUuid={logUuid}
+        setRecord={setRecord}
+        />
         
         {selected && <Button type="primary" htmlType="submit">Next Page</Button>}
         {!selected && <p>After searching for your home, please hit <b>Select</b> next to your property</p>}
