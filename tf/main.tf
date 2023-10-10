300948ce4d3c027d383ea83b2f04db0fa5906b50
pjsier <pjsier@gmail.com>
2023-10-10 18:07:13 -0400

    Add RDS instance

--- tf/main.tf
+++ tf/main.tf
@@ -339,3 +339,116 @@ module "apigw" {
 
   tags = local.tags
 }
+data "aws_availability_zones" "available" {}
+
+locals {
+  vpc_cidr = "10.0.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+}
+
+resource "aws_ssm_parameter" "db_name" {
+  name = "/${local.name}/database/name"
+  type = "SecureString"
+
+  value = "ptap"
+}
+
+resource "aws_ssm_parameter" "db_username" {
+  name = "/${local.name}/database/username"
+  type = "SecureString"
+
+  value = "ptap"
+}
+
+resource "aws_ssm_parameter" "db_password" {
+  name = "/${local.name}/database/password"
+  type = "SecureString"
+
+  value = "ptap1234"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = local.name
+  cidr = local.vpc_cidr
+
+  azs              = local.azs
+  public_subnets   = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
+  private_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 3)]
+  database_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 6)]
+
+  create_database_subnet_group           = true
+  create_database_subnet_route_table     = true
+  create_database_internet_gateway_route = true
+
+  tags = local.tags
+}
+
+module "security_group" {
+  source  = "terraform-aws-modules/security-group/aws//modules/postgresql"
+  version = "~> 5.0"
+
+  name                = local.name
+  vpc_id              = module.vpc.vpc_id
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  tags = local.tags
+}
+
+module "rds" {
+  source  = "terraform-aws-modules/rds/aws"
+  version = "6.1.1"
+
+  identifier = local.name
+
+  engine               = "postgres"
+  engine_version       = "14"
+  family               = "postgres14"
+  major_engine_version = "14"
+  instance_class       = "db.t4g.micro"
+
+  allocated_storage     = 5
+  max_allocated_storage = 20
+
+  db_name  = aws_ssm_parameter.db_name.value
+  username = aws_ssm_parameter.db_username.value
+  port     = 5432
+
+  multi_az               = false
+  db_subnet_group_name   = module.vpc.database_subnet_group
+  vpc_security_group_ids = [module.security_group.security_group_id]
+  publicly_accessible    = true
+  ca_cert_identifier     = "rds-ca-rsa4096-g1"
+
+  maintenance_window              = "Mon:00:00-Mon:03:00"
+  backup_window                   = "03:00-06:00"
+  enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
+  create_cloudwatch_log_group     = true
+
+  backup_retention_period = 1
+  skip_final_snapshot     = true
+  deletion_protection     = false
+
+  performance_insights_enabled          = true
+  performance_insights_retention_period = 7
+  create_monitoring_role                = true
+  monitoring_interval                   = 60
+
+  apply_immediately = true
+
+  parameters = [
+    {
+      name  = "autovacuum"
+      value = 1
+    },
+    {
+      name  = "client_encoding"
+      value = "utf8"
+    }
+  ]
+
+  monitoring_role_name            = "${local.name}-rds-monitoring-role-name"
+  monitoring_role_use_name_prefix = true
+}
