2643efc167b550db25152b99836879a8cc8aa8af
pjsier <pjsier@gmail.com>
2023-12-10 13:46:40 -0500

    Update font size

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from "@vitejs/plugin-react"
 export default defineConfig({
   base: process.env.VITE_PUBLIC_URL || "/",
   plugins: [react(), splitVendorChunkPlugin()],
+  css: {
+    preprocessorOptions: {
+      less: {
+        modifyVars: { "font-size-base": "16px" },
+        javascriptEnabled: true,
+      },
+    },
+  },
 })
