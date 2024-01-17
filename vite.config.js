b4dc3919813c4662d5e8f0dd4165be308fbfaedc
pjsier <pjsier@gmail.com>
2024-01-17 07:51:41 -0500

    Add sentry sourcemaps

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig, splitVendorChunkPlugin } from "vite"
 import react from "@vitejs/plugin-react"
+import { sentryVitePlugin } from "@sentry/vite-plugin"
 
 export default defineConfig({
   base: process.env.VITE_PUBLIC_URL || "/",
-  plugins: [react(), splitVendorChunkPlugin()],
+  build: { sourcemap: true },
   css: {
     preprocessorOptions: {
       less: {
@@ -12,4 +13,13 @@ export default defineConfig({
       },
     },
   },
+  plugins: [
+    react(),
+    splitVendorChunkPlugin(),
+    sentryVitePlugin({
+      org: "ptap",
+      project: "ptap",
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    }),
+  ],
 })
