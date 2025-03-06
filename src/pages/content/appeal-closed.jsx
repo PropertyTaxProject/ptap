b66e87b526451ed834c7155f9ee54d3b97bde025
pjsier <pjsier@gmail.com>
2025-03-05 19:31:20 -0500

    refactor: close detroit app for 2025

--- src/pages/content/appeal-closed.jsx
+++ src/pages/content/appeal-closed.jsx
@@ -2,23 +2,31 @@ import React from "react"
 import { Space } from "antd"
 import PropTypes from "prop-types"
 import coalition from "../../assets/coalition_logo.png"
+import udmLaw from "../../assets/udmlaw.png"
 import ilo from "../../assets/ilo.png"
 
 const AppealClosed = ({ region }) => (
   <>
     <h1>Who We Are</h1>
     <p>
-      The Coalition for Property Tax Justice is the current campaign of the
-      Institute for Law and Organizing (ILO). We are a nonprofit community
-      organization. In addition to Detroit, ILO is working in Milwaukee,
-      Chicago, and nationally to end racialized property tax administration in
-      America.
+      The Institute for Law and Organizing and the Coalition for Property Tax
+      Justice are nonprofit community organizations, which have partnered with
+      the University of Detroit Mercy School of Law Housing Law Clinic to create
+      the Detroit Property Tax Appeal Program. This program helps Detroit
+      homeowners determine whether or not the City of Detroit has illegally
+      inflated their property taxes. If so, we partner with homeowners to file a
+      property tax assessment appeal to correct this, FREE OF CHARGE.
     </p>
     <Space wrap direction="horizontal">
       <img
         src={ilo}
         style={{ height: "80px" }}
         alt="Institute for Law and Organizing logo"
+      />
+      <img
+        src={udmLaw}
+        style={{ height: "80px" }}
+        alt="University of Detroit Mercy Law logo"
       />
       <img
         src={coalition}
@@ -30,14 +38,14 @@ const AppealClosed = ({ region }) => (
     <br />
     {region === "detroit" && (
       <p>
-        Our 2024 application period for the 2024 Property Tax Appeals Program
+        Our 2025 application period for the 2025 Property Tax Appeals Program
         has closed. If you believe your home is over assessed, you can still
         appeal your property tax assessment to the Board of Review by submitting
         an application using{" "}
         <a href="https://detroitmi.gov/government/boards/property-assessment-board-review">
           these instructions
         </a>{" "}
-        by March 11.
+        by March 10.
       </p>
     )}
   </>
